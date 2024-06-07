b1023fa35d39761111a2072005267c26c395cf1f
Rodrigo Mendonça <rod_igomail@hotmail.com>
2024-06-07 09:26:06 -0300

    Criando a classe Posicao

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Program
+    {
+        static void Main()
+        {
+            Posicao p;
+
+            p = new(3, 4);
+
+            Console.WriteLine("Posição: " + p);
+
+        }
+    }
+}
---
+++ tabuleiro/Posicao.cs
@@ -0,0 +1,13 @@
+﻿namespace tabuleiro
+{
+    class Posicao(int linha, int coluna)
+    {
+        public int Linha { get; set; } = linha;
+        public int Coluna { get; set; } = coluna;
+
+        public override string ToString()
+        {
+            return Linha + ", " + Coluna;
+        }
+    }
+}
